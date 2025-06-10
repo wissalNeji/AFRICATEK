439557922e634eef1b8b0f8a664c1e9566dfa054
wissalNeji <44780166+wissalNeji@users.noreply.github.com>
2025-06-10 22:23:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import io
+
+st.title("AI-Powered Data Cleaner & Summarizer")
+st.write("This app will clean your data and generate a summary.")
+uploaded_file = st.file_uploader("📤 Upload your CSV file", type=["csv"])
+if uploaded_file:
+  df = pd.read_csv(uploaded_file)
+  df.fillna(df.mean(numeric_only=True), inplace=True)
+  st.subheader("👀 Preview of Uploaded Data")
+  st.dataframe(df.head())
+  st.subheader("📊 Dataset Info")
+  buffer = io.StringIO()
+  df.info(buf=buffer)
+  s = buffer.getvalue()
+  st.text(s)
+    # Display descriptive statistics
+  st.subheader("📈 Summary Statistics")
+  st.dataframe(df.describe())
